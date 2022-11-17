685c9190046122eae3fcef89f6876cfc3f5e8a2c
Camilo Estrada <camilo.estrada@bpt.com.co>
2022-11-16 20:22:43 -0500

    Se listan los clientes

--- machine_shop/lib/screens/lista_screen.dart
+++ machine_shop/lib/screens/lista_screen.dart
@@ -1,10 +1,6 @@
 import 'dart:convert';
-import 'dart:ffi';
 
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:machine_shop/helpers/constants.dart';
 import 'package:machine_shop/models/cliente.dart';
 import 'package:machine_shop/screens/formulario_creacion_cliente.dart';
@@ -34,79 +30,25 @@ class _ListaState extends State<Lista> {
   List<Cliente> _clientes = [];
   @override
   Widget build(BuildContext context) {
-    _listarClientes();
+    //_listarClientes();
     return Scaffold(
       appBar: AppBar(
-        title: const Text('AppBar Demo'),
-        actions: <Widget>[
-          IconButton(
-            icon: const Icon(Icons.add_alert),
-            tooltip: 'Show Snackbar',
-            onPressed: () {
-              ScaffoldMessenger.of(context).showSnackBar(
-                  const SnackBar(content: Text('This is a snackbar')));
-            },
-          ),
-          IconButton(
-            icon: const Icon(Icons.navigate_next),
-            tooltip: 'Go to the next page',
-            onPressed: () {
-              Navigator.push(context, MaterialPageRoute<void>(
-                builder: (BuildContext context) {
-                  return Scaffold(
-                    appBar: AppBar(
-                      title: const Text('Next page'),
-                    ),
-                    body: const Center(
-                      child: Text(
-                        'This is the next page',
-                        style: TextStyle(fontSize: 24),
-                      ),
-                    ),
-                  );
-                },
-              ));
-            },
-          ),
-        ],
+        title: const Text('Listado de clientes'),
       ),
       backgroundColor: Color.fromARGB(255, 194, 199, 226),
-      body: Stack(children: <Widget>[_listaBuilder(), _showButtonAdd()]),
-    );
-  }
-
-  Widget _listaBuilder() {
-    print(_clientes);
-    return ListView.builder(
-      itemBuilder: (_, i) {
-        String name = _clientes[i].nombre;
-        String cedula = _clientes[i].cedula;
-        return Card(
-          child: ListTile(
-            leading: FlutterLogo(),
-            title: Text(name),
-            subtitle: Text(cedula),
-            trailing: PopupMenuButton(
-              itemBuilder: (context) {
-                return [
-                  PopupMenuItem(
-                    value: 'Editar',
-                    child: Text('Editar'),
-                  ),
-                  PopupMenuItem(
-                    value: 'eliminar',
-                    child: Text('Eliminar'),
-                  )
-                ];
-              },
-              onSelected: (String value) {
-                print('You Click on po up menu item');
-              },
-            ),
-          ),
-        );
-      },
-      itemCount: _clientes.length,
+      body: Stack(children: <Widget>[
+         FutureBuilder(
+            future: _dataList(),
+            builder: (_, AsyncSnapshot snapshot) {
+              if (!snapshot.hasData) {
+                return CircularProgressIndicator();
+              }
+              return ListView(
+                children: snapshot.data,
+              );
+            }),
+        _showButtonAdd()
+        ]),
     );
   }
 
@@ -125,21 +67,51 @@ class _ListaState extends State<Lista> {
     );
   }
 
-  void _listarClientes() async {
+  Future<List<Cliente>> _listarClientes() async {
     var url = Uri.parse('${Constants.apiUrl}clientes');
- 
+
     var response = await http.get(
-      url, 
+      url,
       headers: {
         "Access-Control-Allow-Origin": "*",
         'Content-Type': 'application/json',
         'Accept': '*/*'
       },
     );
-
     var body = jsonDecode(response.body);
     var lista =
         List<Cliente>.from(body.map((model) => Cliente.fromJson(model)));
     _clientes = lista;
+    return _clientes;
+  }
+
+  Future<List<Widget>> _dataList() async {
+    final List<Cliente> clinetesResponse = await _listarClientes();
+    return clinetesResponse
+        .map((n) => Card(
+          child: ListTile(
+            leading: FlutterLogo(),
+            title: Text(n.nombre),
+            subtitle: Text(n.cedula),
+            trailing: PopupMenuButton(
+              itemBuilder: (context) {
+                return [
+                  PopupMenuItem(
+                    value: 'Editar',
+                    child: Text('Editar'),
+                  ),
+                  PopupMenuItem(
+                    value: 'eliminar',
+                    child: Text('Eliminar'),
+                  )
+                ];
+              },
+              onSelected: (String value) {
+                print('You Click on po up menu item');
+              },
+            ),
+          ),
+        )   )
+        .toList();
   }
 }
